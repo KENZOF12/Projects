6eb87649e7a76a1b16e1f3e3349983b7f06bb926
KENZOF12 <felipeishitani@gmail.com>
2026-06-29 17:26:05 -0400

    Add commands for water conservation tips

---
+++ main.py
@@ -0,0 +1,11 @@
+@bot.command()
+async def water(ctx):
+    await ctx.send("Para economizar agua, você deve garantir que torneira e chuveiros não estejam vazando, antes de continuar com qualquer coisa que iria fazer.")
+
+@bot.command()
+async def sacola(ctx):
+    await ctx.send("Sempre busque usar sacolas sacolas renovaveis para preservar o meio ambiente.")
+
+@bot.command()
+async def lixo(ctx):
+    await ctx.send("Para evitar o acúmulo de lixo, você deve priorizar o conssumo de forma consciente.")
